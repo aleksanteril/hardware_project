ed79567c5e4f9568840cc06621d82917a7239451
Aleksanteri Laitinen <aleksanteri.laitinen@metropolia.fi>
2025-04-17 15:09:47 +0300

    Changed the PPI calculation to use ticks_ms instead of sample nums

--- multithreadexample.py
+++ multithreadexample.py
@@ -3,6 +3,7 @@ from ssd1306 import SSD1306_I2C
 import _thread
 from fifo import Fifo
 from piotimer import Piotimer
+import time
 
 class Screen(SSD1306_I2C):
       def __init__(self, da, cl):
@@ -25,8 +26,9 @@ class Screen(SSD1306_I2C):
             return
       
       def draw_bpm(self, bpm):
+            text = f"AVG BPM: {bpm:.0f}"
             self.fill_rect(0, 32, 128, 32, 0)
-            self.text(f"AVG BPM: {bpm:.0f}", 0, 48, 1)
+            self.text(text, 0, 48, 1)
             return
             
 class Isr_adc:
@@ -58,88 +60,82 @@ def scale(sample, max_list, scale_fc):
       calc_y = int(round(pos))
       return calc_y
 
-def calculate_ppi(peak_a, peak_b):
-      #Calculate interval and conversion to milliseconds
-      ppi = (peak_a - peak_b)*4
-
-      if ppi > 2000 or ppi < 250: #Discard if not realistic
-            return
-      
-      PPI.append(ppi)
-      if len(PPI) > 10:
-            del PPI[0]
-
-      calculate_bpm()
-    
 def calculate_bpm():
-      global avg_bpm
+      if len(PPI) < 10:
+            return 0
       #Avg bpm calculation
       avg_ppi = sum(PPI) / len(PPI)
       avg_bpm = 60000 / avg_ppi
+      return avg_bpm
+
+def raw_filter(ppi):
+      if ppi > 2000 or ppi < 250:
+            return
+      PPI.append(ppi)
+      if len(PPI) > 10:
+            del PPI[0]
       return
 
-def find_peaks():
-      global sample_num
+def find_ppi(sample):
       global MARGIN
       global edge
-      global current_peak
-      global previous_peak
+      global peak_time
+      global prev_peak_time
 
-      sample_num += 1
-      threshold = (sum(samples) / len(samples))*1.05*MARGIN
+      threshold = (sum(samples) / len(samples))*MARGIN*1.05
 
       #If signal under threshold and no new peak, ignore and get new sample
-      if samples[-1] < threshold and not edge:
+      if sample < threshold and not edge:
             return
 
       #Rising edge detected
-      if samples[-1] > threshold and not edge:
+      elif sample > threshold and not edge:
+            peak_time = time.ticks_ms()
+            raw_filter(time.ticks_diff(peak_time, prev_peak_time))
             edge = True
-            current_peak = sample_num
-            MARGIN = 0.9
+            MARGIN = 0.95
             return
 
       #If under threshold and new rise was detected, do calculations and reset
-      if samples[-1] < threshold and edge:
-            calculate_ppi(current_peak, previous_peak)
-            previous_peak = current_peak
+      elif sample < threshold and edge:
+            prev_peak_time = peak_time
             MARGIN = 1
             edge = False
             return
 
 
 def core0_thread():
-      global count
       global x
       global max_list
       global min_list
       global scale_fc
       global sample_num
-      global avg_bpm
 
       #When 250 samples has been read, calculate the new scale factor to draw with the old 250
       if sample_num % 250 == 0:
             max_list, min_list, scale_fc = calculate_scale_factor(samples[:250])
 
       #Take interval amount of samples, replace old samples to keep the list between 250.
-      while count < 5:
-            if data.empty():
-                  continue
-            sample = data.get()
-            del samples[0]
-            samples.append(sample)
-            find_peaks()
-            count += 1
-      count = 0
+      if data.empty():
+            return
+      sample = data.get()
+      sample_num += 1
 
+      del samples[0]
+      samples.append(sample)
+      
+      find_ppi(sample)
+      avg_bpm = calculate_bpm()
+      screen.draw_bpm(avg_bpm)
 
+      if sample_num % 5 != 0:
+            return
       #Calculate and draw every 5th sample and scale to plot
       calc_y = scale(sample, max_list, scale_fc)
       calc_y = min(max(0, calc_y), 31) #Limit y between screen
       
       screen.hr_px_coords(x, calc_y)
       screen.draw_hr()
-      screen.draw_bpm(avg_bpm)
       x += 1
       if x > screen.width:
             x = 0
@@ -157,20 +153,17 @@ max_list = 0
 min_list = 0
 scale_fc = 0
 sample_num = 0
-count = 0
 x = 0
-avg_bpm = 0
 
 #For drawing and hr reading
 samples = []
 
 #For hr reading
 MARGIN = 1
-sample_num = 0
-current_peak = 0
-previous_peak = 0
 edge = False
 PPI = []
+peak_time = time.ticks_ms()
+prev_peak_time = time.ticks_ms()
 
 
 #Get 500 samples for the threshold, and also scaling
