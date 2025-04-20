de78a8afbd9f9e61f01bf6c80b67ab95fb1c31b8
jarior <jari.orkolainen@metropolia.fi>
2025-04-20 19:03:35 +0000

    singletöity.

--- connect_wlan_mqtt.py
+++ connect_wlan_mqtt.py
@@ -5,53 +5,12 @@ from umqtt.simple import MQTTClient
 
 # Connection class for WLAN and MQTT
 class Connection:
-    '''
-    eka vaihtoehto = https://www.youtube.com/watch?v=Awoh5-Yr6SE
     _instance = None
-
-    def __init__(self):
-        raise RuntimeError("this is a singleton, invoke get_instance() instead")
-    @classmethod
-    def get_instance(cls):
-        if cls._instance==None:
-            cls._instance = cls.__new__(cls)
-        return cls._instance
-    def log(self, ex: Exception):
-        print(ex)
-
-    def log(self,message: str):
-        print(message) 
-    '''
-
-    '''
-    toka vaihtoehto = https://www.geeksforgeeks.org/singleton-pattern-in-python-a-complete-guide/
-    def __new__(cls):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(Connection, cls).__new__(cls)
-    return cls.instance
-  
- singleton = Connection()
- new_singleton = Connection()
-
- print(singleton is new_singleton)
-
- singleton.singl_variable = "Singleton Variable"
- print(new_singleton.singl_variable)
-    '''
-
-    '''
-    kolmas vaihtoehto = chatgpt <3
- _instance = None
-    def __new__(cls):
+    def __new__(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance = super(Singleton, cls).__new__(cls)
+            cls._instance = super().__new__(cls)
         return cls._instance
-
- # Usage
- obj1 = Singleton()
- obj2 = Singleton()
- print(obj1 is obj2)  # Will print True, as both are the same instance
-    '''
+    
     def __init__ (self, SSID, PWD, IP):
         self.SSID = SSID
         self.PWD = PWD
