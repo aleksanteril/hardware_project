fec940e853cd3ace72c915d77af845fb3c6cc487
Xaruthh <jesse.mustonen@hotmail.fi>
2025-04-22 16:14:37 +0300

    online file lisätty send local mikä toimii mutta ku  send kubios toimii mutta se ei vielä anna oikeata palautusta kun sinne subscribee MUISTAA SITTEN DATAAN TIMESTAMP EI TIME MUUTEN EI TALLENNA NETTISIVULLE tämä koodi on melkein valmis, vain kubios data send ja response toimivuus puuttuu

--- library/online.py
+++ library/online.py
@@ -1,6 +1,7 @@
 import network
 import ntptime
 import time
+import ujson
 from umqtt.simple import MQTTClient
 
 # Connection class for WLAN and MQTT
@@ -12,21 +13,23 @@ class Online:
             cls._instance = super().new(cls)
         return cls._instance
     
-    def __init__(self, SSID, PWD, IP):
+    def __init__(self, SSID: str, PWD: str, IP: str):
         self.SSID = SSID
         self.PWD = PWD
         self.IP = IP
         self.connected = False
-        self.mqtt_client = None
+        self.local_mqtt = None
+        self.kubios_mqtt = None
         
         # Connect to WLAN and MQTT automatically on object creation
         if self.connect_wlan():
             ntptime.host = "fi.pool.ntp.org"
             ntptime.settime()
-            self.connect_mqtt()
+            self.local_mqtt = self.connect_mqtt('local', 1883)
+            self.kubios_mqtt = self.connect_mqtt('kubios', 21883)
         print(time.localtime())
 
-    def connect_wlan(self):  # Connect to WLAN method, try 10 times.
+    def connect_wlan(self) -> bool:  # Connect to WLAN method, try 10 times.
         self.wlan = network.WLAN(network.STA_IF)
         self.wlan.active(True)
         self.wlan.connect(self.SSID, self.PWD)
@@ -43,27 +46,56 @@ class Online:
         print("WLAN connection successful. IP:", self.wlan.ifconfig()[0])
         return True
     
-    def connect_mqtt(self):  # Connect to MQTT
+    # 21883 is the kubios port, and 1883 is the local port for HR-data
+    def connect_mqtt(self, id, port):  # Connect to MQTT
         try:
-            self.mqtt_client = MQTTClient("client_id", self.IP)
-            self.mqtt_client.connect(clean_session=True)
-            print("MQTT connection successful!")
+            client = MQTTClient(id, self.IP, port)
+            client.connect(clean_session=True)
+            print(f"MQTT connection to {port} successful!")
             self.connected = True
+            return client
         except Exception as e:
             print(f"Failed to connect to MQTT: {e}")
-            self.mqtt_client = None
+            return None
 
-    def send_mqtt_message(self, topic, message):  # Try sending a message when method is called upon. If no connection, give error.
-        if self.mqtt_client is not None:
+
+    def send_mqtt_message(self, client, topic, data: list):  # Try sending a message when method is called upon. If no connection, give error.
+        if client is not None:
             try:
-                self.mqtt_client.publish(topic, message)
-                print(f"Sending to MQTT: {topic} -> {message}")
+                client.publish(topic, data)
+                print(f"Sending to MQTT: {topic} -> {data}")
             except Exception as e:
                 print(f"Failed to send MQTT message: {e}")
         else:
             print("MQTT client not connected!")
             
-    def is_connected(self):
+    def is_connected(self) -> bool:
         return self.connected
 
+    def send_kubios(self, data: dict) -> dict:
+        data = ujson.dumps(data)
+        self.kubios_mqtt.subscribe('kubios-response', data)
+        self.send_mqtt_message(self.kubios_mqtt,'kubios-request', data)
+    
+    def send_local(self, data: dict):
+        data = ujson.dumps(data)
+        self.send_mqtt_message(self.kubios_mqtt,'hr-data', data)
+        
+
+k_data = {
+    "id": 787,
+    "type": "RRI",
+    "data": [828, 836, 852, 760, 800, 796, 856, 824, 808, 776, 724, 816, 800, 812, 812, 812, 756, 820, 812, 800],
+    "analysis": { "type": "readiness"}
+  }
+
+hr_data = {
+    "id": 123,
+    "timestamp": 123456789,
+    "mean_hr": 78
+  }
+
 connect = Online("KMD657_Group_1", "ykasonni123", "192.168.1.253")
+
+connect.send_local(hr_data)
+
